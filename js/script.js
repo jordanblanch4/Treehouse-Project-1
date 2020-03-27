db808da2de24cc447804d36ad0e78c8220e861fd
Jordan Blanchard <jordanb17b@icloud.com>
2020-03-27 15:56:05 -0700

    started to build JS functionality
    built base of product.

--- js/script.js
+++ js/script.js
@@ -10,21 +10,42 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+const quotes = [
+  {Quote: "All I wanted to do was be a golden millionaire." , Source: "Dinesh", Citation: "Silicon Valley"},
+  {Quote: "What’s happening? I’m sorry, what? Mr. Car", Source:"Jared", Year: "2014"}, 
+  {Quote: "Nobody exists on purpose. Nobody belongs anywhere. We're all going to die. Come watch TV.", Source: "Morty"}, 
+  {Quote: 'Weddings are basically funerals with cake.', Source: 'Rick'},
+  {Quote: 'If it was, you could call me Ernest Hemingway.', Source: 'Beth'}
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote(quoteList) {
+  const randGen = Math.floor(Math.random() * quoteList.length);
+  return quoteList[randGen];
+};
 
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+  var quoteOBJ = getRandomQuote(quotes);
+  let HTML = '<p class="quote">'+quoteOBJ.Quote+'</p>';
+  HTML+= '<p class="source">'+quoteOBJ.Source;
+  if(quotes.Citation !== undefined) {
+    HTML+= '<span class="citation">'+quoteOBJ.Citation+'</span>';
+  }
+    if(quotes.Year !== undefined) {
+      HTML+='<span class="year">'+quoteOBJ.Year+'</span>';
+  }
+    HTML+='</p>';
+return HTML;
+};
 
-
-
+document.getElementById('quote-box').innerHTML = printQuote();
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
